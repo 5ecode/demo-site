455111b0b120154752c9fcc765f4c71ad27653a8
5ecode <70137301+5ecode@users.noreply.github.com>
2026-08-31 18:02:30 +0900

    [update] 気になっていたところを諸々改修

--- src/js/contact/index.js
+++ src/js/contact/index.js
@@ -1,32 +1,37 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------------
 @送信許可
 ----------------------------------------------------------------------*/
 (function () {
-  const stage = document.getElementById('contactForm'),
-    agreeTarget = document.getElementById('agreeCheck'),
-    submitBtn = document.getElementById('submitBtn'),
-    requiredTarget = stage.querySelectorAll(':required'),
+  const stage = document.getElementById("contactForm"),
+    agreeCheck = document.getElementById("agreeCheck"),
+    agreeTarget = agreeCheck.querySelector(":required"),
+    confirmBtn = stage.querySelector('[name="submitConfirm"]'),
+    requiredTarget = stage.querySelectorAll(":required"),
     param = {
-      pattern: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
+      pattern:
+        /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/,
       check: false,
-      errorColor : '#f00'
+      errorColor: "#f00",
     };
 
   const func = {
     init: () => {
-      func.checkRequired();
-      func.checkAgree();
+      if (confirmBtn) {
+        func.checkRequired();
+        func.checkAgree();
+        func.checkTotal();
+      }
     },
 
     checkRequired: () => {
       for (var i = 0; i < requiredTarget.length; i++) {
-        requiredTarget[i].addEventListener('blur', (e) => {
+        requiredTarget[i].addEventListener("blur", (e) => {
           const target = e.target;
 
-          if (e.target.value !== '') {
-            target.style.borderColor = '';
-            if (target.type === 'email') {
+          if (e.target.value !== "") {
+            target.style.borderColor = "";
+            if (target.type === "email") {
               func.checkMail(target);
             }
           } else {
@@ -39,14 +44,14 @@
 
     checkMail: (target) => {
       if (param.pattern.test(target.value)) {
-        target.style.borderColor = ''
+        target.style.borderColor = "";
       } else {
         target.style.borderColor = param.errorColor;
       }
     }, //checkMail
 
     checkAgree: () => {
-      agreeTarget.addEventListener('change', () => {
+      agreeTarget.addEventListener("change", () => {
         if (agreeTarget.checked === true) {
           param.check = true;
         } else {
@@ -58,11 +63,11 @@
 
     checkTotal: () => {
       if (stage.checkValidity() && param.check) {
-        submitBtn.disabled = false;
+        confirmBtn.disabled = false;
       } else {
-        submitBtn.disabled = true;
+        confirmBtn.disabled = true;
       }
-    } //checkTotal
-  }
+    }, //checkTotal
+  };
   return func.init();
 })();
