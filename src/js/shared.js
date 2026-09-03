3ce1b7a90dcf24fbd163132a5701f794246ca876
5ecode <70137301+5ecode@users.noreply.github.com>
2026-09-03 10:09:35 +0900

    [update] タブ移動対応

--- src/js/shared.js
+++ src/js/shared.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------------
 @ResizeManager : リサイズ
 ----------------------------------------------------------------------*/
@@ -12,7 +12,7 @@ class ResizeManager {
     let timer,
       lastInnerWidth = window.innerWidth,
       _this = this;
-    window.addEventListener('resize', function () {
+    window.addEventListener("resize", function () {
       if (lastInnerWidth != window.innerWidth) {
         if (timer) return;
         timer = setTimeout(function () {
@@ -29,21 +29,40 @@ class ResizeManager {
 @gNavToggle : グローバルメニュー
 ----------------------------------------------------------------------*/
 const gNavToggle = (() => {
-  const target = document.getElementById('Header'),
-  trigger = document.getElementById('menuTrigger'),
-  board = document.getElementById('menuBoard'),
-  param = {
-    activeClass: 'is-active',
-    fixedClass : 'is-fixed',
-    modalBody : 'u-isModalBody'
-  };
+  const target = document.getElementById("Header"),
+    trigger = target.querySelector('[data-menu="trigger"]'),
+    logo = target.querySelector("[data-header-logo]"),
+    board = document.getElementById("menuBoard"),
+    menuElms = target.querySelectorAll('[data-menu="board"]'),
+    focusTargetElms = target.querySelectorAll("[data-menu]"),
+    param = {
+      activeClass: "is-active",
+      fixedClass: "is-fixed",
+      modalBody: "u-isModalBody",
+    };
 
   const func = {
     init: () => {
+      func.setTabIndex(false);
       func.triggerEvent();
       var responsive = new ResizeManager(func.resize);
       responsive.resize();
     }, //init
+
+    // [setTabIndex プロパティ切替]
+    setTabIndex: (active) => {
+      menuElms.forEach((item) => {
+        if (active) {
+          logo.tabIndex = -1;
+          if (item.getAttribute("href") !== "#") {
+            item.tabIndex = 0;
+          }
+        } else {
+          item.tabIndex = -1;
+          logo.tabIndex = 0;
+        }
+      });
+    },
 
     adjustScreen: (fixed) => {
       const scrollbarWidth = window.innerWidth - document.body.clientWidth;
@@ -57,7 +76,7 @@ const gNavToggle = (() => {
     }, //adjustScreen
 
     // [triggerEvent トリガーイベント]
-    triggerEvent : () => {
+    triggerEvent: () => {
       document.body.onclick = (e) => {
         param.state = target.classList.contains(param.activeClass);
         if (e.target === trigger) {
@@ -78,23 +97,63 @@ const gNavToggle = (() => {
     menuOpen: () => {
       func.adjustScreen(true);
       target.classList.add(param.activeClass);
-      document.getElementsByTagName('html')[0].classList.add(param.fixedClass);
+      document.getElementsByTagName("html")[0].classList.add(param.fixedClass);
+      func.setTabIndex(true);
+      func.focus(true);
     }, //menuOpen
 
-
     // [menuClose メニューを閉じる]
-    menuClose : () => {
+    menuClose: () => {
       target.classList.remove(param.activeClass);
       setTimeout(() => {
         func.adjustScreen(false);
-        document.getElementsByTagName('html')[0].classList.remove(param.fixedClass);
-      },80)
+        document
+          .getElementsByTagName("html")[0]
+          .classList.remove(param.fixedClass);
+      }, 80);
+      func.setTabIndex(false);
+      func.focus(false);
     }, //menuClose
 
+    focus: (status) => {
+      //メニューボードを開いたときはフォーカスを循環
+      document.addEventListener("keydown", (e) => {
+        if (status) {
+          const activeEl = document.activeElement;
+          const firstEl = focusTargetElms[0];
+          const lastEl = focusTargetElms[focusTargetElms.length - 1];
+
+          //タブキーが押されたか
+          const tabKey = 9 === e.keyCode;
+          //シフトキーが押されたか
+          const shiftKey = e.shiftKey;
+          //Escapeキーが押されたか
+          const escKey = 27 === e.keyCode;
+
+          //最後の要素でタブキーが押された場合、最初の要素にフォーカスを当てる
+          if (!shiftKey && tabKey && lastEl === activeEl) {
+            e.preventDefault();
+            firstEl.focus();
+          }
+          //最初の要素でタブキー+シフトキーが押された場合、最後の要素にフォーカスを当てる
+          if (shiftKey && tabKey && firstEl === activeEl) {
+            e.preventDefault();
+            lastEl.focus();
+          }
+
+          // Escapeキーで閉じる
+          if (escKey) {
+            func.menuClose();
+            firstEl.focus();
+          }
+        }
+      });
+    },
+
     // [resize リサイズ]
-    resize : () => {
+    resize: () => {
       func.menuClose();
-    } //resize
+    }, //resize
   };
   return func.init();
 })();
@@ -104,14 +163,14 @@ const gNavToggle = (() => {
 ----------------------------------------------------------------------*/
 const scrollAnimation = (() => {
   const targets = document.querySelectorAll(".u-appear"),
-        options = {
-          root: null,
-          rootMargin: "-30% 0px",
-          threshold: 0
-        },
-        param = {
-          showclass: 'is-show'
-        };
+    options = {
+      root: null,
+      rootMargin: "-30% 0px",
+      threshold: 0,
+    },
+    param = {
+      showclass: "is-show",
+    };
   const func = {
     init: function () {
       const observer = new IntersectionObserver(func.callback, options);
@@ -130,6 +189,6 @@ const scrollAnimation = (() => {
         object.unobserve(cont);
       });
     }, //callback
-  }
+  };
   return func.init();
 })();
